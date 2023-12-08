57dc1fef067dc19cb70a80adc3b56bb003af31db
Qilin Xie <qxie@gicw.org>
2023-12-08 09:54:59 -0800

    completes timelines, adds banner

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-  document.addEventListener('DOMContentLoaded', function () {
-            const timelineItems = document.querySelectorAll('.animate-in');
+document.addEventListener('DOMContentLoaded', function () {
+    const timelineItems = document.querySelectorAll('.animate-in');
 
-            function checkPosition() {
-                timelineItems.forEach((item) => {
-                    const position = item.getBoundingClientRect().top;
-                    const windowHeight = window.innerHeight;
+    function checkPosition() {
+        timelineItems.forEach((item) => {
+            const position = item.getBoundingClientRect().top;
+            const windowHeight = window.innerHeight;
 
-                    if (position < windowHeight * 0.9) {
-                        item.classList.add('show');
-                    }
-                });
+            if (position < windowHeight * 0.9) {
+                item.classList.add('show');
             }
-
-            window.addEventListener('scroll', checkPosition);
-            window.addEventListener('resize', checkPosition);
-            checkPosition(); // Trigger the check on page load
         });
+    }
+
+    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('resize', checkPosition);
+    checkPosition(); // Trigger the check on page load
+});
